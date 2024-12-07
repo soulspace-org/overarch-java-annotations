ad7c05b78d397c127a0021b3c85db3eb2f22d989
Ludger Solbach <soulman@soulspace.org>
2024-12-07 13:58:16 +0100

    released version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.soulspace/overarch-java-annotations "0.1.0-SNAPSHOT"
+(defproject org.soulspace/overarch-java-annotations "0.1.0"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
